4fa5976bca346f5ece46fca6b19be72ad773250b
Estecka <estecka@galacsys.com>
2023-11-21 00:19:12 +0100

    Updated generator to accept multiple variants at once

--- datagen.sh
+++ datagen.sh
@@ -8,26 +8,26 @@ function dry_run(){
 }
 
 function cut(){
-	export OUTPUT="$ID_NAMESPACE:$OUT"
-	export  INPUT="$ID_NAMESPACE:$IN"
-	envsubst <./templates/cut.json >"./data/$ID_NAMESPACE/recipes/${OUT}_from_${IN}_stonecutting.json"
+	export OUTPUT="$NAMESPACE:$OUT"
+	export  INPUT="$NAMESPACE:$IN"
+	envsubst <./templates/cut.json >"./data/$NAMESPACE/recipes/${OUT}_from_${IN}_stonecutting.json"
 }
 
 function uncraft(){
-	export OUTPUT="$ID_NAMESPACE:$OUT"
-	export  INPUT="$ID_NAMESPACE:$IN"
-	envsubst <"./templates/craft_$COST.json" >"./data/$ID_NAMESPACE/recipes/uncraft/${IN}_$COST.json"
+	export OUTPUT="$NAMESPACE:$OUT"
+	export  INPUT="$NAMESPACE:$IN"
+	envsubst <"./templates/craft_$COST.json" >"./data/$NAMESPACE/recipes/uncraft/${IN}_$COST.json"
 }
 
 function recipes(){
 	case $1 in
-	_slab)
+	%s_slab)
 	IN=$RAW OUT=$VAR COUNT=2 cut;
 	IN=$VAR OUT=$RAW COUNT=1 COST="2h" uncraft
 	IN=$VAR OUT=$RAW COUNT=2 COST="4" uncraft
 	;;
 
-	_brick_fence)
+	%s_brick_fence)
 	IN=$RAW OUT=$VAR COUNT=1 cut;
 	IN=$VAR OUT=$RAW COUNT=3 COST="4" uncraft
 	;;
@@ -39,47 +39,27 @@ function recipes(){
 	esac
 }
 
-function idsplit(){
-	if [[ "$1" =~ ^(.+):(.+)$ ]]
-	then
-		ID_NAMESPACE=${BASH_REMATCH[1]}
-		ID_PATH=${BASH_REMATCH[2]}
-		return 0;
-	else
-		return 1;
-	fi;
-}
-
 function generate(){
-	radical=$1;
-	raw_suffix=$2
-	var_suffix=$3
-	idsplit "$radical" || return 1;
+	export NAMESPACE=$1
+	local radical=$2
+	local raw_form=$3
+	shift 3;
 
-	RAW="$ID_PATH$raw_suffix";
-	VAR="$ID_PATH$var_suffix";
+	export RAW=$(printf "$raw_form" "$radical");
+	export GROUP="$NAMESPACE:$RAW";
 
-
-	echo >&2 "$ID_NAMESPACE:$ID_PATH $raw_suffix <- $var_suffix"
-	export ID_NAMESPACE;
-	export VAR;
-	export RAW;
-	export GROUP="$ID_NAMESPACE:$RAW";
-	dry_run && return 0;
-	mkdir -p "./data/$ID_NAMESPACE/recipes/uncraft"
-	recipes "$var_suffix";
-}
-
-# dry_run || rm -r ./data
-
-function test(){
-	while [[ $# -gt 0 ]];
+	dry_run || mkdir -p "./data/$NAMESPACE/recipes/uncraft";
+	while [[ $# -gt 0 ]]
 	do
-		echo -n ":$1 "
+		local var_form=$1
+		export VAR=$(printf "$var_form" "$radical");
 		shift;
+		echo >&2 "$NAMESPACE:$RAW <-> $NAMESPACE:$VAR"
+		dry_run || recipes "$var_form";
 	done;
-	echo
 }
+
+dry_run || rm -r ./data
 
 jq <$DATA -c '
 	to_entries[] 
@@ -88,8 +68,8 @@ jq <$DATA -c '
 ' | while read -r entry;
 do
 	echo "$entry" | jq '.[]' -r | tr -d '\r'| {
-		args=()
+		args=();
 		while read -r a; do args+=("$a"); done
-		test "${args[@]}";
+		generate "${args[@]}";
 	};
 done;
